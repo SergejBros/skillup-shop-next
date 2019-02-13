cacb1025251f3fd5049f6a3f1c8804d6b2fd8557
Ros333 <lisogorrostislav@gmail.com>
2019-02-13 21:29:40 +0200

    подготовка к экзамену

--- src/Controller/ViewPostController.php
+++ src/Controller/ViewPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,12 @@ class ViewPostController extends AbstractController
     /**
      * @Route("/viewpost/{id}", name="view_post")
      */
-    public function index()
+    public function index($id, PostRepository $postRepository)
     {
+        $viewPost = $postRepository->find($id);
+
         return $this->render('view_post/index.html.twig', [
-            'controller_name' => 'ViewPostController',
+            'viewPost' => $viewPost,
         ]);
     }
 }
